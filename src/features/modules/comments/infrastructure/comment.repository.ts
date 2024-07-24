f4026b01b476efcbfab829d6d800b6d2846b4b22
Ermuhamet <ermyhamet2002@gmail.com>
2024-07-24 16:53:49 +0600

    Added delete method in comment repository

--- src/features/modules/comments/infrastructure/comment.repository.ts
+++ src/features/modules/comments/infrastructure/comment.repository.ts
@@ -1,17 +1,90 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Comment, CommentDocument } from '../domain/comment.entity';
-import {
-  CommentLikes,
-  CommentLikesDocument,
-} from '../../../likes/domain/like.entity';
+//import { Comment, CommentDocument } from '../domain/comment.entity';
 import { InjectDataSource } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
+import { Comment } from '../domain/comment.sql.entity';
+import { CommentLikes } from '../../../likes/domain/like.sql.entity';
 
 @Injectable()
 export class CommentRepository {
   constructor(@InjectDataSource() private dataSource: DataSource) {}
+  async createComment(comment: Comment) {
+    const query = `
+      INSERT INTO "Comments" ("postIdFk", "content", "userIdFk", "createdAt")
+      VALUES ($1, $2, $3, $4)
+      RETURNING "commentId";
+    `;
+    const parameters = [
+      comment.postIdFk,
+      comment.content,
+      comment.userIdFk,
+      comment.createdAt,
+    ];
+
+    const result = await this.dataSource.query(query, parameters);
+
+    return result[0].commentId;
+  }
+  async find(commentId: string) {
+    const query = `
+      SELECT 
+        c."commentId",
+        c."postIdFk",
+        c."content",
+        c."createdAt",
+        u."userId",
+        u."login"
+      FROM "Comments" c
+      JOIN "Users" u ON c."userIdFk" = u."userId"
+      WHERE c."commentId" = $1;
+    `;
+    const parameters = [commentId];
+
+    const result = await this.dataSource.query(query, parameters);
+    if (result.length === 0) {
+      return null;
+    }
+    console.log('Comment after find in database: ', result[0]);
+    return result[0];
+  }
+  async updateLikeStatus(updatedLikeStatusDto: CommentLikes) {
+    const query = `
+      INSERT INTO "CommentLikes" ("authorId", "parentId", "status", "createdAt")
+      VALUES ($1, $2, $3, $4)
+      ON CONFLICT ("authorId", "parentId")
+      DO UPDATE SET "status" = EXCLUDED."status"
+      RETURNING "likeId", "authorId", "parentId", "status", "createdAt";
+    `;
+    const parameters = [
+      updatedLikeStatusDto.authorId,
+      updatedLikeStatusDto.parentId,
+      updatedLikeStatusDto.status,
+      updatedLikeStatusDto.createdAt,
+    ];
+    try {
+      const result = await this.dataSource.query(query, parameters);
+      return result[0];
+    } catch (error) {
+      console.error('Error updating comment like:', error);
+      throw error;
+    }
+  }
+  async deleteCommentById(commentId: string) {
+    const deleteQuery = `
+      DELETE FROM "Comments"
+      WHERE "commentId" = $1
+      RETURNING "commentId";
+    `;
+    const parameters = [commentId];
+    try {
+      const result = await this.dataSource.query(deleteQuery, parameters);
+      return result.length > 0;
+    } catch (error) {
+      throw new Error(`Failed to delete comment with error: ${error}`);
+    }
+  }
   /*constructor(
     @InjectModel(Comment.name) private commentModel: Model<CommentDocument>,
     @InjectModel(CommentLikes.name)
